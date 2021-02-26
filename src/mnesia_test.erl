1c6e10de5c3eb8d676cf0b2754210e06777153fd
Oleg Butnaru <oleg.butnaru@mail.ru>
2021-02-26 18:37:57 +0200

    Adjust code

--- src/mnesia_test.erl
+++ src/mnesia_test.erl
@@ -12,7 +12,9 @@
 -include_lib("stdlib/include/qlc.hrl").
 
 %% API
--export([init/0, insert_job/2, insert_employee/4, read/2, delete/2, update/3, read_all/0, read/4, read/1, update_job/3, read_all_jobs/0]).
+-export([init/0, insert_job/2, insert_employee/4,
+  read/1, read/2, delete/2, update/3,
+  read_all/0, update_job/3, read_all_jobs/0]).
 
 
 init() ->
@@ -35,11 +37,8 @@ insert_job(Name, Department) ->
   mnesia:transaction(fun() ->
     mnesia:match_object(job, #job{_ = '_'}, write) end).
 
-
-%%test() -> mnesia_test:insert_employee("Ana", 25, "Moldova", "Tester").
 insert_employee(Name, Age, Location, Job_Name) ->
   EmployeeRecord = fun() ->
-%%    Index = #job{id = '$1', _ = '_'},
     JobRecord = get_job_record(Job_Name),
     EmployeeId = mnesia:dirty_update_counter(erlang_sequence, employee, 1),
     Record = #employee{
@@ -55,24 +54,14 @@ insert_employee(Name, Age, Location, Job_Name) ->
   mnesia:transaction(fun() ->
     mnesia:match_object(employee, #employee{_ = '_'}, write) end).
 
-get_job_record(Job_Name) ->
-  Q = qlc:q([J || J <- mnesia:table(job), J#job.name =:= Job_Name]),
-  Ev = qlc:e(Q),
-  get_record(Ev).
-
-get_record(Ev) ->
-  if
-    Ev =:= [] -> job_do_not_exist;
-    true -> hd(Ev)
-  end.
-
+%% Query element by any key/s where L = [{key1, value1}, {key2, value2}, ...]
 read(L) when is_list(L) ->
   MatchHead = #employee{id = '$1', name = '$2', age = '$3', location = '$4', job = #job{name = '$5', department = '$6', _ = '_'}},
   Guard = get_guard_list(L),
   Result = '$_',
   mnesia:activity(transaction, fun() -> mnesia:select(employee, [{MatchHead, Guard, [Result]}], write) end).
 
-
+%% Query with one key-value
 read(Key, Value) ->
   case Key of
     id -> mnesia:activity(transaction, fun() -> mnesia:read(employee, Value, write) end);
@@ -80,7 +69,6 @@ read(Key, Value) ->
       {_, L} = mnesia:transaction(fun() -> mnesia:match_object(employee, #employee{name = Value, _ = '_'}, write) end),
       L;
     age ->
-%%    mnesia:transaction(fun() -> mnesia:read(employee, #employee{age = Value, _ ='_'}, write) end);
       MatchHead = #employee{age = '$1', _ = '_'},
       Guard = [{'>=', '$1', Value}],
       Result = '$_',
@@ -96,30 +84,10 @@ read(Key, Value) ->
         mnesia:match_object(employee, #employee{job = #job{department = Value, _ = '_'}, _ = '_'}, write) end), L
   end.
 
-read(Key1, Value1, Key2, Value2) ->
-  case {Key1, Key2} of
-    {age, job} ->
-      Fun = fun() ->
-        MatchHead = #employee{age = '$1', job = #job{name = '$2', _ = '_'}, _ = '_'},
-        Guard = [{'>=', '$1', Value1}, {'==', '$2', Value2}],
-        Result = '$_',
-        mnesia:select(employee, [{MatchHead, Guard, [Result]}], write) end,
-      mnesia:activity(transaction, Fun);
-    {name, location} ->
-      Fun = fun() ->
-        MatchHead = #employee{name = '$1', location = '$2', _ = '_'},
-        Guard = [{'=:=', '$1', Value1}, {'=:=', '$2', Value2}],
-        Result = '$_',
-        mnesia:select(employee, [{MatchHead, Guard, [Result]}], write) end,
-      mnesia:activity(transaction, Fun);
-    _ -> ok
-  end.
-
 delete(Key, Value) ->
   L = read(Key, Value),
   case Key of
     id -> mnesia:transaction(fun() -> mnesia:delete(employee, Value, write) end);
-
     age ->
       DeleteFun = fun(X) -> mnesia:delete_object(X) end,
       mnesia:transaction(fun() -> lists:foreach(DeleteFun, L) end);
@@ -141,45 +109,7 @@ update_job(Id, Key, NewValue) when is_integer(Id) ->
   UpdateJobFun = fun() ->
     OldJob = mnesia:activity(transaction, fun() -> mnesia:read(job, Id, write) end),
     over_write_job(OldJob, Key, NewValue) end,
-    mnesia:activity(transaction, UpdateJobFun).
-
-over_write([E | _], Key, NewValue) ->
-  case Key of
-    name -> New = E#employee{name = NewValue},
-      mnesia:write(New);
-    age -> New = E#employee{age = NewValue},
-      mnesia:write(New);
-    location ->
-      New = E#employee{location = NewValue},
-      mnesia:write(New);
-    job -> New = E#employee{job = get_job_record(NewValue)},
-      mnesia:write(New)
-  end;
-over_write([], _, _) -> ok.
-
-%%over_write([E | Tail], Key, NewValue) ->
-%%  case Key of
-%%    name -> New = E#employee{name = NewValue},
-%%      mnesia:write(New),
-%%      1 + over_write(Tail, Key, NewValue);
-%%    age -> New = E#employee{age = NewValue},
-%%      mnesia:write(New),
-%%      1 + over_write(Tail, Key, NewValue);
-%%    location ->
-%%      New = E#employee{location = NewValue},
-%%      mnesia:write(New),
-%%      1 + over_write(Tail, Key, NewValue);
-%%    job -> New = E#employee{job = get_job_record(NewValue)},
-%%      mnesia:write(New),
-%%      1 + over_write(Tail, Key, NewValue)
-%%  end;
-
-
-get_delete_action(L) ->
-  case L of
-    [] -> ok;
-    _ -> mnesia:transaction(fun() -> mnesia:delete_object(employee, hd(L), write) end)
-  end.
+  mnesia:activity(transaction, UpdateJobFun).
 
 read_all() ->
   mnesia:transaction(fun() ->
@@ -188,6 +118,21 @@ read_all() ->
 read_all_jobs() ->
   mnesia:transaction(fun() ->
     mnesia:match_object(job, #job{_ = '_'}, write) end).
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+get_job_record(Job_Name) ->
+  Q = qlc:q([J || J <- mnesia:table(job), J#job.name =:= Job_Name]),
+  Ev = qlc:e(Q),
+  get_record(Ev).
+
+get_record(Ev) ->
+  if
+    Ev =:= [] -> job_do_not_exist;
+    true -> hd(Ev)
+  end.
 
 get_guard_list(L) -> get_all_keys(L, []).
 
@@ -207,6 +152,44 @@ get_key_place(K) ->
     job -> '$5';
     department -> '$6'
   end.
+
+get_delete_action(L) ->
+  case L of
+    [] -> ok;
+    _ -> mnesia:transaction(fun() -> mnesia:delete_object(employee, hd(L), write) end)
+  end.
+
+over_write([E | _], Key, NewValue) ->
+  case Key of
+    name -> New = E#employee{name = NewValue},
+      mnesia:write(New);
+    age -> New = E#employee{age = NewValue},
+      mnesia:write(New);
+    location ->
+      New = E#employee{location = NewValue},
+      mnesia:write(New);
+    job -> New = E#employee{job = get_job_record(NewValue)},
+      mnesia:write(New)
+  end;
+over_write([], _, _) -> ok.
+
+%% The same function but for multiple elements
+%%over_write([E | Tail], Key, NewValue) ->
+%%  case Key of
+%%    name -> New = E#employee{name = NewValue},
+%%      mnesia:write(New),
+%%      1 + over_write(Tail, Key, NewValue);
+%%    age -> New = E#employee{age = NewValue},
+%%      mnesia:write(New),
+%%      1 + over_write(Tail, Key, NewValue);
+%%    location ->
+%%      New = E#employee{location = NewValue},
+%%      mnesia:write(New),
+%%      1 + over_write(Tail, Key, NewValue);
+%%    job -> New = E#employee{job = get_job_record(NewValue)},
+%%      mnesia:write(New),
+%%      1 + over_write(Tail, Key, NewValue)
+%%  end;
 
 over_write_job([J | _], Key, NewValue) ->
   case Key of
